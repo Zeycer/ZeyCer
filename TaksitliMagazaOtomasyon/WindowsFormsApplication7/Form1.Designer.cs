b1a0a57a026ba54e4475dc308781129451e5d4ae
Zeynep Ceren MEMİŞ <135732259+Zeycer@users.noreply.github.com>
2023-07-07 11:27:16 +0300

    Taksitli Mağaza Otomasyonu

---
+++ TaksitliMagazaOtomasyon/WindowsFormsApplication7/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace WindowsFormsApplication7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 39);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(736, 127);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Müşteriler Tablosu";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 294);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(733, 126);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(429, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Taksit Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(264, 245);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ödeme Tarihi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ödeme Tutarı :";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(164, 208);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(88, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Müşteri Adı Soyadı";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(329, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "label5";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(535, 243);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Borçlular lİstesi";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 448);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TaksitliMagazaOtomasyon/WindowsFormsApplication7/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+//using System.Data.OleDb;  Access veritabanı için
+
+
+
+namespace WindowsFormsApplication7
+{
+    public partial class Form1 : Form
+    {
+
+        //global Değişkenler
+        SqlConnection baglanti = new SqlConnection("Data Source=MAINBILMUHLAB;Initial Catalog=magaza;Integrated Security=True");
+        SqlCommand komut = new SqlCommand();
+        int musterino;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            komut.CommandText = "select * from musteriler";
+            DataTable dt = new DataTable();
+            komut.Connection = baglanti;
+            SqlDataReader dr;
+            baglanti.Open();
+            dr = komut.ExecuteReader();
+            dt.Load(dr);
+            dataGridView1.DataSource = dt;
+            baglanti.Close();
+            dt.Dispose();
+            dr.Dispose();
+            komut.Dispose();
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            komut.CommandText = "taksitekle";
+            komut.CommandType = CommandType.StoredProcedure;
+            komut.Parameters.AddWithValue("@odemetarih", dateTimePicker1.Value);
+            komut.Parameters.AddWithValue("@odemetutar", textBox2.Text);
+            komut.Parameters.AddWithValue("@musterino", musterino);
+            //komut.Parameters["@musterino"].Direction = ParameterDirection.Output;
+            komut.Connection = baglanti;
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            komut.Parameters.Clear();
+            komut.CommandType = CommandType.Text;
+            baglanti.Close();
+
+            taksitlerigoster();
+
+        }
+
+        public void taksitlerigoster()
+        {
+            komut.CommandText = "taksitlerigetir";
+            komut.Parameters.Add("@musno", SqlDbType.Int);
+            komut.Parameters["@musno"].Value = musterino;
+            komut.CommandType = CommandType.StoredProcedure;
+
+            komut.Connection = baglanti;
+            DataTable dt = new DataTable();
+            baglanti.Open();
+            dt.Load(komut.ExecuteReader());
+            dataGridView2.DataSource = dt;
+            baglanti.Close();
+            //baglanti.Dispose();
+            komut.CommandType = CommandType.Text;
+            komut.Parameters.Clear();
+            komut.Dispose();
+            dt.Dispose();
+        
+        }
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            musterino = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+            label4.Text = Convert.ToString(musterino);
+
+
+
+
+
+            komut.CommandText = "toplamborc";
+            komut.Parameters.Add("@musno", SqlDbType.Int);
+            komut.Parameters["@musno"].Value = musterino;
+            komut.Parameters.Add("@borc", SqlDbType.Money);
+            komut.Parameters["@borc"].Direction = ParameterDirection.Output;
+            komut.CommandType = CommandType.StoredProcedure;
+
+            komut.Connection = baglanti;
+            DataTable dt = new DataTable();
+            baglanti.Open();
+            dt.Load(komut.ExecuteReader());
+            dataGridView2.DataSource = dt;
+
+            label5.Text = "Toplam Borç = " + komut.Parameters["@borc"].Value.ToString();
+            baglanti.Close();
+            komut.CommandType = CommandType.Text;
+            komut.Parameters.Clear();
+            komut.Dispose();
+            dt.Dispose();
+
+            taksitlerigoster();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            komut.CommandText = "borclularlistesi";
+            komut.Parameters.Add("@odemetarih", SqlDbType.Date);
+            komut.Parameters["@odemetarih"].Value = DateTime.Now.Date;
+            komut.CommandType = CommandType.StoredProcedure;
+
+            komut.Connection = baglanti;
+            DataTable dt = new DataTable();
+            baglanti.Open();
+            dt.Load(komut.ExecuteReader());
+            dataGridView2.DataSource = dt;
+            baglanti.Close();
+            //baglanti.Dispose();
+            komut.CommandType = CommandType.Text;
+            komut.Parameters.Clear();
+            komut.Dispose();
+            dt.Dispose();
+        }
+
+
+
+
+    }
+}
